cb02fc959f792ad8deb76b117419fe5f608c94bf
Andrew Watts <ahwatts@gmail.com>
2011-11-09 15:31:49 -0500

    Add a rough guess at how the EventMachine stuff should work.

--- lib/mogilefsd_log_tailer.rb
+++ lib/mogilefsd_log_tailer.rb
@@ -1,7 +1,33 @@
 require 'mogilefsd_log_tailer/version'
 require 'optparse'
+require 'eventmachine'
 
 class MogilefsdLogTailer
+  module Handler
+    def post_init
+      @received_data = ''
+    end
+
+    def receive_data(data)
+      port, ip = Socket.unpack_sockaddr_in(get_peername)
+      puts("Received #{data.inspect} from #{ip}:#{port}")
+      if data =~ /\n/
+        lines = data.split("\n")
+        @received_data << lines.first
+        print_log_entry(@received_data, ip, port)
+        lines[1..-2].each { |l| print_log_entry(l, ip, port) }
+        @received_data = lines.last
+      else
+        @received_data << data
+      end
+      puts("@received_data = #{@received_data}")
+    end
+
+    def print_log_entry(line, ip, port)
+      puts("Printing line: #{line.inspect} from #{ip}:#{port}")
+    end
+  end
+
   def initialize
     @hosts = []
   end
@@ -14,9 +40,23 @@ class MogilefsdLogTailer
       o.separator ""
       o.on_tail("-h", "--help", "Show this help message.") { puts o; exit }
     end.parse!
+
+    while (h = ARGV.shift)
+      @hosts << h
+    end
+
+    if @hosts.empty?
+      STDERR.puts("No hosts to tail.")
+      exit(1)
+    end
   end
 
   def run
-    p @hosts
+    EventMachine.run do
+      @hosts.each do |hp|
+        host, port = hp.split(':')
+        EventMachine.connect(host, port.to_i, Handler)
+      end
+    end
   end
 end
