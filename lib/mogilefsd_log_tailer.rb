353fc461c305064d31e67847167e632f81fabc66
Andrew Watts <ahwatts@gmail.com>
2011-11-09 14:46:10 -0500

    Put an option parser in.

--- lib/mogilefsd_log_tailer.rb
+++ lib/mogilefsd_log_tailer.rb
@@ -1,5 +1,22 @@
-require "mogilefsd_log_tailer/version"
+require 'mogilefsd_log_tailer/version'
+require 'optparse'
 
-module MogilefsdLogTailer
-  # Your code goes here...
+class MogilefsdLogTailer
+  def initialize
+    @hosts = []
+  end
+
+  def parse_options!
+    OptionParser.new do |o|
+      script_name = File.basename($0)
+      o.set_summary_indent('  ')
+      o.banner = "#{script_name} version #{VERSION}\nUsage: #{script_name} [options] tracker1:port1 tracker2:port2 ..."
+      o.separator ""
+      o.on_tail("-h", "--help", "Show this help message.") { puts o; exit }
+    end.parse!
+  end
+
+  def run
+    p @hosts
+  end
 end
--- lib/mogilefsd_log_tailer/version.rb
+++ lib/mogilefsd_log_tailer/version.rb
@@ -1,3 +1,3 @@
-module MogilefsdLogTailer
+class MogilefsdLogTailer
   VERSION = "0.0.1"
 end
